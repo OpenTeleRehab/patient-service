6aa29d532a84b4f9b45089e0668c7061fc6cf8b8
Chanthorn Ngeth <chanthorn@web-essentials.asia>
2020-12-02 11:29:44 +0700

    [IMP] Implement api for get users
    refs TAR-42

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'identity' => $this->identity,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'phone' => $this->phone,
+            'clinic_id' => $this->clinic_id,
+            'country_id' => $this->country_id,
+            'date_of_birth' => $this->date_of_birth,
+            'note' => $this->note,
+            'gender' => $this->gender,
+        ];
+    }
+}
