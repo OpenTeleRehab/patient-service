17d8618ade3babb9989117a302bb1b11ccdcddc0
Phearoth <phearoth@web-essentials.co>
2021-04-02 15:28:05 +0700

    [TASK] Implement Admin Dashboard APIs
    refs TRA-11

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\PatientController;
 use App\Http\Controllers\TreatmentPlanController;
 use \App\Http\Controllers\AppointmentController;
+use \App\Http\Controllers\ChartController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,7 @@ Route::get('patient/list/by-therapist-id', [PatientController::class, 'getByTher
 Route::post('auth/login', [AuthController::class, 'login']);
 Route::post('auth/add-new-pin', [AuthController::class, 'addNewPinCode']);
 Route::get('treatment-plan/get-treatment-plan-detail', [TreatmentPlanController::class, 'getActivities']);
+Route::get('chart/get-data-for-global-admin', [ChartController::class, 'getDataForGlobalAdmin']);
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('auth/logout', [AuthController::class, 'logout']);
