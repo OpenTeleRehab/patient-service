57a403e61c4b049901418bf27a2be19c8fcc49b8
Lina <lina@web-essentials.co>
2021-06-21 09:07:23 +0700

    [BUGFIX] Fix ongoing treatment over limit
    refs TRA-592

--- app/Models/TreatmentPlan.php
+++ app/Models/TreatmentPlan.php
@@ -10,7 +10,6 @@ class TreatmentPlan extends Model
     const STATUS_PLANNED = 'planned';
     const STATUS_ON_GOING = 'on_going';
     const STATUS_FINISHED = 'finished';
-    const NUMBER_OF_ONGOING_TREATMENT_LIMIT = 'number_of_ongoing_treatment_per_therapist';
     const PAIN_THRESHOLD_LIMIT = 'pain_threshold_limit';
 
     /**
