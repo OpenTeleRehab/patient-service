449e2450d813aa4d1246b503f20bbaee7633abb1
Lina Sun <lina@web-essentials.co>
2025-04-22 13:54:45 +0700

    [IMP] Improve patient raw data report
    refs TRA-1023

--- app/Http/Resources/PatientRawDataTreatmentPlanResource.php
+++ app/Http/Resources/PatientRawDataTreatmentPlanResource.php
@@ -16,52 +16,86 @@ class PatientRawDataTreatmentPlanResource extends JsonResource
      */
     public function toArray($request)
     {
-        $activities = $this->activities()
-        ->selectRaw('
-            day,
-            week,
-            COUNT(*) as number_of_exercise, 
-            SUM(completed) as number_of_completed_exercise, 
-            SUM(pain_level) as total_pain_level,
-            SUM(CASE WHEN pain_level IS NOT NULL THEN 1 ELSE 0 END) as number_of_submitted_pain_level
-        ')
+        $lastDayOfTreatment = $this->activities()
+        ->where('week', $this->total_of_weeks)
+        ->where('treatment_plan_id', $this->id)
+        ->max('day');
+
+        $initialAverageAdherence = $this->activities()
         ->where('type', 'exercise')
-        ->groupBy('day')
-        ->groupBy('week')
-        ->get();
+        ->where('day', 1)
+        ->where('week', 1)
+        ->avg('completed');
+
+        $initialAveragePainLevel = $this->activities()
+        ->where('type', 'exercise')
+        ->where('day', 1)
+        ->where('week', 1)
+        ->avg('pain_level');
+
+        $finalAverageAdherence = $this->activities()
+        ->where('type', 'exercise')
+        ->where('day', $lastDayOfTreatment)
+        ->where('week', $this->total_of_weeks)
+        ->avg('completed');
+
+        $finalAveragePainLevel = $this->activities()
+        ->where('type', 'exercise')
+        ->where('day', $lastDayOfTreatment)
+        ->where('week', $this->total_of_weeks)
+        ->avg('pain_level');
         
         $dailyGoalIds = $this->goals()->where('frequency', Goal::FREQUENCY_DAILY)->pluck('id')->toArray();
         $weeklyGoalIds = $this->goals()->where('frequency', Goal::FREQUENCY_WEEKLY)->pluck('id')->toArray();
-        $weeklyGoals = $this->activities()
-        ->selectRaw('
-            week,
-            ANY_VALUE(satisfaction) as satisfaction,
-            SUM(completed) as number_of_submitted_goal
-        ')
+
+        $initialAverageDailyGoal = $this->activities()
+        ->where('type', 'goal')
+        ->whereIn('activity_id', $dailyGoalIds)
+        ->where('day', 1)
+        ->where('week', 1)
+        ->avg('satisfaction');
+
+        $finalAverageDailyGoal = $this->activities()
+        ->where('type', 'goal')
+        ->whereIn('activity_id', $dailyGoalIds)
+        ->where('day', $lastDayOfTreatment)
+        ->where('week', $this->total_of_weeks)
+        ->avg('satisfaction');
+
+        $initialAverageWeeklyGoal = $this->activities()
         ->where('type', 'goal')
         ->whereIn('activity_id', $weeklyGoalIds)
-        ->groupBy('week')
-        ->get();
+        ->where('week', 1)
+        ->avg('satisfaction');
 
-        $dailyGoals = $this->activities()
-        ->selectRaw('
-            day,
-            ANY_VALUE(satisfaction) as satisfaction,
-            SUM(completed) as number_of_submitted_goal
-        ')
+        $finalAverageWeeklyGoal = $this->activities()
+        ->where('type', 'goal')
+        ->whereIn('activity_id', $weeklyGoalIds)
+        ->where('week', $this->total_of_weeks)
+        ->avg('satisfaction');
+
+        $averageWeeklyGoal = $this->activities()
+        ->where('type', 'goal')
+        ->whereIn('activity_id', $weeklyGoalIds)
+        ->avg('satisfaction');
+
+        $averageDailyGoal = $this->activities()
         ->where('type', 'goal')
         ->whereIn('activity_id',$dailyGoalIds)
-        ->groupBy('day')
-        ->get();
+        ->avg('satisfaction');
 
         $questionnaires = $this->activities()
             ->where('type', Activity::ACTIVITY_TYPE_QUESTIONNAIRE)
             ->where('completed', 1)
+            ->orderBy('week')
+            ->orderBy('day')
             ->get();
 
         $questionnaires = $questionnaires->map(function ($questionnaire) {
             return [
                 'id' => $questionnaire->activity_id,
+                'week' => $questionnaire->week,
+                'day' => $questionnaire->day,
                 'answer' => QuestionnaireAnswerResource::collection($questionnaire->answers),
             ];
         });
@@ -74,13 +108,20 @@ class PatientRawDataTreatmentPlanResource extends JsonResource
             'start_date' => $this->start_date ? $this->start_date->format(config('settings.date_format')) : '',
             'end_date' => $this->end_date ? $this->end_date->format(config('settings.date_format')) : '',
             'status' => $this->status,
-            'weeklyGoals' => $weeklyGoals,
-            'dailyGoals' => $dailyGoals,
-            'activities' => $activities,
+            'averageWeeklyGoal' => $averageWeeklyGoal,
+            'averageDailyGoal' => $averageDailyGoal,
             'total_of_weeks' => $this->total_of_weeks,
             'created_by' => $this->created_by,
             'disease_id' => $this->disease_id,
             'questionnaires' => $questionnaires,
+            'initialAverageAdherence' => $initialAverageAdherence,
+            'initialAveragePainLevel' => $initialAveragePainLevel,
+            'finalAverageAdherence' => $finalAverageAdherence,
+            'finalAveragePainLevel' => $finalAveragePainLevel,
+            'initialAverageDailyGoal' => $initialAverageDailyGoal,
+            'initialAverageWeeklyGoal' => $initialAverageWeeklyGoal,
+            'finalAverageDailyGoal' => $finalAverageDailyGoal,
+            'finalAverageWeeklyGoal' => $finalAverageWeeklyGoal,
         ];
     }
 }
