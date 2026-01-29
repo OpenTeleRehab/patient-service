4c5c49bb9906e6417d667c3b3e9fe64f5d5fa439
Sophea Phy <sophea@web-essentials.co>
2026-01-29 14:13:16 +0700

    [TASK] Broadcast referral notification
    refs TRA-1127

---
+++ app/Mail/PatientReferralMail.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Forwarder;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class PatientReferralMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $subject;
+    public $content;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(string $prefix, string $recipientName, string $actionName, int $languageId)
+    {
+        $endpoint = env('ADMIN_SERVICE_URL') . '/email-templates/'. $prefix .'/get-by-prefix';
+        $accessToken = Forwarder::getAccessToken(Forwarder::ADMIN_SERVICE);
+
+        $template = Http::withToken($accessToken)->get($endpoint, [
+            'lang' => $languageId,
+        ]);
+
+        if ($template->successful()) {
+            $template = $template->json()['data'];
+
+            $this->subject = $template['title'];
+            $this->content = $template['content'];
+
+            // Replace email content.
+            $this->content = str_replace('#user_name#', $recipientName, $this->content);
+            $this->content = str_replace('#healthcare_worker_name#', $actionName, $this->content);
+            $this->content = str_replace('#rehab_service_admin_name#', $actionName, $this->content);
+            $this->content = str_replace('#therapist_name#', $actionName, $this->content);
+        }
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: $this->subject,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.referral',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
--- app/Models/ReferralAssignment.php
+++ app/Models/ReferralAssignment.php
@@ -2,7 +2,10 @@
 
 namespace App\Models;
 
+use App\Mail\PatientReferralMail;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
 
 class ReferralAssignment extends Model
 {
@@ -18,9 +21,102 @@ class ReferralAssignment extends Model
     protected $fillable = [
         'referral_id',
         'therapist_id',
+        'accepted_by',
         'status',
         'reason',
     ];
+
+    /**
+     * Bootstrap the model and its traits.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::created(function ($assignment) {
+            $access_token = Forwarder::getAccessToken(Forwarder::THERAPIST_SERVICE);
+
+            $therapist = Http::withToken($access_token)->get(env('THERAPIST_SERVICE_URL') . '/therapist/by-id', [
+                'id' => $assignment->therapist_id,
+            ])->throw();
+
+            $healthcareWorker = Http::withToken($access_token)->get(env('THERAPIST_SERVICE_URL') . '/therapist/by-id', [
+                'id' => $assignment->referral->phc_worker_id,
+            ])->throw();
+
+            // TODO: Fetch rehab service admin info.
+            $rehabServiceAdmin = Http::withToken($access_token)->get(env('THERAPIST_SERVICE_URL') . '/therapist/by-id', [
+                'id' => $assignment->therapist_id,
+            ])->throw();
+
+            if ($therapist->successful() && $rehabServiceAdmin->successful()) {
+                $therapist = $therapist->json();
+
+                // TODO: check user notifiable
+                Mail::to($therapist['email'])->send(
+                    new PatientReferralMail(
+                        'new-assigned-patient-referral-request-from-a-rehab-service-admin',
+                        $therapist['last_name'] . ' ' . $therapist['first_name'],
+                        $rehabServiceAdmin['last_name'] . ' ' . $rehabServiceAdmin['first_name'],
+                        $therapist['language_id'],
+                    )
+                );
+            }
+
+            if ($healthcareWorker->successful() && $rehabServiceAdmin->successful()) {
+                $healthcareWorker = $healthcareWorker->json();
+
+                // TODO: check user notifiable.
+                Mail::to($healthcareWorker['email'])->send(
+                    new PatientReferralMail(
+                        'rehab-service-admin-assigns-the-patient-referral-request',
+                        $healthcareWorker['last_name'] . ' ' . $healthcareWorker['first_name'],
+                        $rehabServiceAdmin['last_name'] . ' ' . $rehabServiceAdmin['first_name'],
+                        $healthcareWorker['language_id'],
+                    )
+                );
+            }
+        });
+
+        self::updated(function ($assignment) {
+            $therapistAccessToken = Forwarder::getAccessToken(Forwarder::THERAPIST_SERVICE);
+            $adminAccessToken = Forwarder::getAccessToken(Forwarder::ADMIN_SERVICE);
+
+            $healthcareWorker = Http::withToken($therapistAccessToken)->get(env('THERAPIST_SERVICE_URL') . '/therapist/by-id', [
+                'id' => $assignment->referral->phc_worker_id,
+            ])->throw();
+
+            $therapist = Http::withToken($therapistAccessToken)->get(env('THERAPIST_SERVICE_URL') . '/therapist/by-id', [
+                'id' => $assignment->therapist_id,
+            ])->throw();
+
+            if ($healthcareWorker->successful() && $therapist->successful()) {
+                $healthcareWorker = $healthcareWorker->json();
+
+                $prefix = $assignment === self::STATUS_ACCEPTED
+                    ? 'therapist-accepts-the-assigned-patient-referral-request-for-healthcare-worker'
+                    : 'therapist-declines-the-assigned-patient-referral-request-for-healthcare-worker';
+
+                Mail::to($healthcareWorker['email'])->send(
+                    new PatientReferralMail(
+                        $prefix,
+                        $healthcareWorker['last_name'] . ' ' . $healthcareWorker['first_name'],
+                        $therapist['last_name'] . ' ' . $therapist['first_name'],
+                        $healthcareWorker['language_id'],
+                    )
+                );
+            }
+
+            // Send notification to rehab service admin.
+            Http::withToken($adminAccessToken)->post(env('ADMIN_SERVICE_URL') . '/notifications/patient-referral-assignment', [
+                'clinic_id' => $assignment->referral->to_clinic_id,
+                'therapist_id' => $assignment->therapist_id,
+                'status' => $assignment->status,
+            ])->throw();
+        });
+    }
 
     /**
      * Get the referral associated with this referral assignment.
